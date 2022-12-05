3e143b5b34c812a1190bf541da258a03235074e2
Matheus Aurélio <matheusaurelio2004@gmail.com>
2022-12-04 23:49:42 -0300

    :sparkles: feat: Adiciona novos componentes

--- mobile/src/components/Loading.tsx
+++ mobile/src/components/Loading.tsx
@@ -1,9 +1,9 @@
 import { Center, Spinner } from "native-base";
 
-export function Loading(){
-    return(
-        <Center flex={1} bgColor="gray.900">
-            <Spinner color="yellow.500" size={32}></Spinner>
-        </Center>
-    )
+export function Loading() {
+  return (
+    <Center flex={1} bg="gray.900">
+      <Spinner color="yellow.500" />
+    </Center >
+  );
 }
